ba25728a852ffefc6ca9a656a77e358e6c7bb9a1
tomitmedia <tomitmedia1@gmail.com>
2021-06-21 02:57:51 +0100

    first

--- BLOGsite/settings.py
+++ BLOGsite/settings.py
@@ -77,16 +77,16 @@ WSGI_APPLICATION = 'BLOGsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(default='sqlite3://tomitmedia:tioluwanimi1@localhost/db.sqlite3')
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite3://tomitmedia:tioluwanimi1@localhost/db.sqlite3')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
